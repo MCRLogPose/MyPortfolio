5252132a99a233d8ca8e81b1f9b5af688331ece5
MCRLogPose <cruzriveraedwinmanuel@gmail.com>
2025-08-24 23:20:21 -0500

    Actualizacion de vistas

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,6 +9,7 @@ const ProjectsPage = () => {
                 <div className="text-white text-center p-10">
                     <h1 className="text-4xl font-bold mb-4">Projects</h1>
                     <p className="text-xl">Here are some of my projects. More coming soon!</p>
+                    <p className="text-xl">Here are some of my projects. More coming soon!</p>
                     
                 </div>
             </VantaBackground>
